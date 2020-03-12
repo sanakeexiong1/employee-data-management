7719a2087cab29982a34008496af83148bc2bef7
Jake Haberle <jake.haberle@gmail.com>
2020-03-11 20:16:13 -0600

    firebase working

--- empapp.js
+++ empapp.js
@@ -1,67 +1,69 @@
-window.onload() function {
-
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyAnXTnCSUHTMAP13PIKc8-dlKyoe0VVo8M",
-    authDomain: "denverbootcampproject.firebaseapp.com",
-    databaseURL: "https://denverbootcampproject.firebaseio.com",
-    projectId: "denverbootcampproject",
-    storageBucket: "denverbootcampproject.appspot.com",
-    messagingSenderId: "572799408700",
-    appId: "1:572799408700:web:3c116edf0fa8d204350139"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-firebase.initializeApp(config);
+// window.onload() function() {
+// Your web app's Firebase configuration
+$(document).ready(function(){
+var firebaseConfig = {
+  apiKey: "AIzaSyAnXTnCSUHTMAP13PIKc8-dlKyoe0VVo8M",
+  authDomain: "denverbootcampproject.firebaseapp.com",
+  databaseURL: "https://denverbootcampproject.firebaseio.com",
+  projectId: "denverbootcampproject",
+  stordateBucket: "denverbootcampproject.appspot.com",
+  messagingSenderId: "572799408700",
+  appId: "1:572799408700:web:3c116edf0fa8d204350139"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 // Create a variable to reference the database.
 var database = firebase.database();
-
 // Initial Values
 var name = "";
-var email = "";
-var age = 0;
-var comment = "";
-
+var role = "";
+var date = "";
+var rate = 0;
 // Capture Button Click
-$("#add-user").on("click", function (event) {
+$("#add-user").on("click", function(event) {
   event.preventDefault();
-
   // Grabbed values from text-boxes
-  name = $("#name-input").val().trim();
-  email = $("#email-input").val().trim();
-  age = $("#age-input").val().trim();
-  comment = $("#comment-input").val().trim();
-
+  name = $("#name-input")
+    .val()
+    .trim();
+  role = $("#Role-input")
+    .val()
+    .trim();
+  date = $("#date-input")
+    .val()
+    .trim();
+  rate = $("#rate-input")
+    .val()
+    .trim();
   // Code for "Setting values in the database"
   // database.ref().push({
-   database.ref().push({
+  database.ref().push({
     name: name,
-    email: email,
-    age: age,
-    comment: comment
+    role: role,
+    date: date,
+    rate: rate
   });
-
 });
-
 // Firebase watcher + initial loader HINT: .on("value")
-database.ref().on("value", function (snapshot) {
-
-  // Log everything that's coming out of snapshot
-  console.log(snapshot.val());
-  console.log(snapshot.val().name);
-  console.log(snapshot.val().email);
-  console.log(snapshot.val().age);
-  console.log(snapshot.val().comment);
-
-  // Change the HTML to reflect
-  $("#name-display").text(snapshot.val().name);
-  $("#email-display").text(snapshot.val().email);
-  $("#age-display").text(snapshot.val().age);
-  $("#comment-display").text(snapshot.val().comment);
-
-  // Handle the errors
-}, function (errorObject) {
-  console.log("Errors handled: " + errorObject.code);
-});
-}
+database.ref().on(
+  "child_added",
+  function(snapshot) {
+    // Log everything that's coming out of snapshot
+    console.log(snapshot.val());
+    console.log(snapshot.val().name);
+    console.log(snapshot.val().role);
+    console.log(snapshot.val().date);
+    console.log(snapshot.val().rate);
+    // Change the HTML to reflect
+    $("#name-display").text(snapshot.val().name);
+    $("#role-display").text(snapshot.val().role);
+    $("#date-display").text(snapshot.val().date);
+    $("#rate-display").text(snapshot.val().rate);
+    // Handle the errors
+  },
+  function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  }
+);
+})
